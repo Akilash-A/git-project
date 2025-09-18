15df73e3bb2932599f2c303a517ae722e6ec589b
Akilash A <akilash.cyber@gmail.com>
2025-09-18 19:51:27 +0530

    make the ui look good

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -25,13 +25,13 @@ export function ThemeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem className="theme-toggle-item" onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem className="theme-toggle-item" onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem className="theme-toggle-item" onClick={() => setTheme("system")}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
